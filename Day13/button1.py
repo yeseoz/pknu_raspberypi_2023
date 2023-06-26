6d89980fa56a802d775d3963ce3503da302748d8
yeseoz <w12345678992@gmail.com>
2023-06-26 15:40:27 +0900

    7세그먼트 번호 띄우기

---
+++ Day13/button1.py
@@ -0,0 +1,33 @@
+#  푸시버튼 예제
+import RPi.GPIO as GPIO
+import time
+
+button = 24
+count = 0
+red = 17
+green = 27
+blue = 22
+
+def clickHndler(channel):
+    global count
+    count = count +1
+    if (count %2 ==0):
+        GPIO.output(red, GPIO.LOW)
+    else:
+        GPIO.output(red, GPIO.HIGH)
+    print(count)
+
+GPIO.setwarnings(False) # 쓸데 없는 경고 표시 로그 사라짐
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(button, GPIO.RISING, callback=clickHndler)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
+
+GPIO.output(red,True)
+GPIO.output(green,True)
+GPIO.output(blue,True) # 여기 까지 초기화
+
+while(True):
+    time.sleep(1)
